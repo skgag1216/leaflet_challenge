feec07f174234b06d5f425b2fc26d023fa355cc6
Stefanie Gagnon <stefaniekathryn@gmail.com>
2022-07-29 15:07:48 -0400

    del extra space at bottom

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -131,10 +131,3 @@ function createFeatures(earthquakeData) {
   legend.addTo(myMap);
   
 };
-
-
-//   let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-// 	maxZoom: 18,
-// 	accessToken: API_KEY
-// });
